bc57dde3fbab037f4572812a3ba436192b63d83c
soni-H <harshitalfs42@gmail.com>
2022-04-19 18:20:58 +0530

    Initial Commit

---
+++ src/test/java/com/example/loanapprover/LoanapproverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanapprover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanapproverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
