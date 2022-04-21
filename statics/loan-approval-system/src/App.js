63c89ffb0e6a919ebce76d2fb654d736666b62c1
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-04-21 13:45:46 +0530

    Frontend added

---
+++ statics/loan-approval-system/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Dashboard from './Components/Dashboard'
+function App() {
+  return (
+    <div>
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ statics/loan-approval-system/src/Components/Dashboard/List.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+function List({ users, handleEdit }) {
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: null
+    });
+    return (
+        <div className='contain-table'>
+            <table className='striped-table'>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Emails</th>
+                        <th>Salary</th>
+                        <th>Date</th>
+                        <th>Loan Status</th>
+                        <th colSpan={2} className="text-center">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.length > 0 ? (
+                        users.map((user, i) => (
+                            <tr key={user.id}>
+                                <td>{i + 1}</td>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                                <td>{user.email}</td>
+                                <td>{formatter.format(user.salary)}</td>
+                                <td>{user.date} </td>
+                                <td>{user.loan_status}</td>
+                                <td className="text-right">
+                                    <button
+                                        onClick={() => handleEdit(user.id)}
+                                        className="button muted-button"
+                                    >
+                                        Edit
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan={7}>No Employees</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+
+export default List
