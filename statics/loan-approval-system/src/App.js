d2535e32ca13bb90c560d2d5e022ed322a76f902
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-04-21 15:31:48 +0530

    Login page added

--- statics/loan-approval-system/src/App.js
+++ statics/loan-approval-system/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from './Components/Dashboard'
+import Login from './Components/Login'
+ 
 function App() {
   return (
-    <div>
-      <Dashboard/>
-    </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+     <Route path="/dashboard" element={<Dashboard auth={false}/>} />
+     <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
+    </Routes>
+  </BrowserRouter>
+    
   );
 }
 
--- statics/loan-approval-system/src/Components/Dashboard/index.js
+++ statics/loan-approval-system/src/Components/Dashboard/index.js
@@ -1,54 +1,57 @@
-import React, {useState} from 'react';
-import {userData} from '../../data'
+import React, { useState } from 'react';
+import { userData } from '../../data'
 import Swal from 'sweetalert2';
 import Header from './Header';
 import Add from './Add';
 import Edit from './Edit';
 import List from './List';
 
-function Dashboard(){
-    const [users, setUsers] = useState(userData);
-    const [selectedUser, setSelectedUser] = useState(null);
-    const [isAdding, setIsAdding] = useState(false);
-    const [isEditing, setIsEditing] = useState(false);
+function Dashboard() {
     
-    const handleEdit = (id) => {
-        const [user] = users.filter(user => user.id === id);
-
-        setSelectedUser(user);
-        setIsEditing(true);
-
-    }
-    return(
-
-        <div className="container">
-            {!isAdding && !isEditing &&  (
-                <>
-                  <Header setIsAdding={setIsAdding}/>
-                  <List users={users} handleEdit={handleEdit}/>
-                </>
-            )}
-            {/* Add */}
-            {isAdding && (
-                <Add
-                    users={users}
-                    setUsers={setUsers}
-                    setIsAdding={setIsAdding} 
-                />
-            )}
-            {isEditing && (
-                <Edit
-                 users={users}
-                 selectedUser={selectedUser}
-                 setUsers={setUsers}
-                 setIsEditing={setIsEditing}
-                /> 
-                    
-            )}
+        const [users, setUsers] = useState(userData);
+        const [selectedUser, setSelectedUser] = useState(null);
+        const [isAdding, setIsAdding] = useState(false);
+        const [isEditing, setIsEditing] = useState(false);
 
 
-        </div>
+        const handleEdit = (id) => {
+            const [user] = users.filter(user => user.id === id);
 
-    )
+            setSelectedUser(user);
+            setIsEditing(true);
+
+        }
+        return (
+
+            <div className="container">
+                {!isAdding && !isEditing && (
+                    <>
+                        <Header setIsAdding={setIsAdding} />
+                        <List users={users} handleEdit={handleEdit} />
+                    </>
+                )}
+                {/* Add */}
+                {isAdding && (
+                    <Add
+                        users={users}
+                        setUsers={setUsers}
+                        setIsAdding={setIsAdding}
+                    />
+                )}
+                {isEditing && (
+                    <Edit
+                        users={users}
+                        selectedUser={selectedUser}
+                        setUsers={setUsers}
+                        setIsEditing={setIsEditing}
+                    />
+
+                )}
+
+
+            </div>
+
+        )
+    
 }
 export default Dashboard;
