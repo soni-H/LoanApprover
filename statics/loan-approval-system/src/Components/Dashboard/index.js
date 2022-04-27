af5b91ddde052d66416938fa6f34b461242134cb
rahulsid55 <90174294+rahulsid55@users.noreply.github.com>
2022-04-27 09:13:17 +0530

    Update index.js

--- statics/loan-approval-system/src/Components/Dashboard/index.js
+++ statics/loan-approval-system/src/Components/Dashboard/index.js
@@ -1,59 +1,34 @@
 import React, { useState } from 'react';
 import { userData } from '../../data'
-import Swal from 'sweetalert2';
 import Header from './Header';
 import Add from './Add';
-import Edit from './Edit';
-import List from './List';
-import {Redirect} from 'react-router-dom';
 
 function Dashboard() {
 
-        
-        const [users, setUsers] = useState(userData);
-        const [selectedUser, setSelectedUser] = useState(null);
-        const [isAdding, setIsAdding] = useState(false);
-        const [isEditing, setIsEditing] = useState(false);
+
+    const [users, setUsers] = useState(userData);
+    const [isAdding, setIsAdding] = useState(false);
+    return (
+
+        <div className="container">
+            {!isAdding && (
+                <>
+                    <Header setIsAdding={setIsAdding} />
+                </>
+            )}
+            {/* Add */}
+            {isAdding && (
+                <Add
+                    users={users}
+                    setUsers={setUsers}
+                    setIsAdding={setIsAdding}
+                />
+            )}
 
 
-        const handleEdit = (id) => {
-            const [user] = users.filter(user => user.id === id);
+        </div>
 
-            setSelectedUser(user);
-            setIsEditing(true);
+    )
 
-        }
-        return (
-
-            <div className="container">
-                {!isAdding && !isEditing && (
-                    <>
-                        <Header setIsAdding={setIsAdding} />
-                        <List users={users} handleEdit={handleEdit} />
-                    </>
-                )}
-                {/* Add */}
-                {isAdding && (
-                    <Add
-                        users={users}
-                        setUsers={setUsers}
-                        setIsAdding={setIsAdding}
-                    />
-                )}
-                {isEditing && (
-                    <Edit
-                        users={users}
-                        selectedUser={selectedUser}
-                        setUsers={setUsers}
-                        setIsEditing={setIsEditing}
-                    />
-
-                )}
-
-
-            </div>
-
-        )
-    
 }
 export default Dashboard;
